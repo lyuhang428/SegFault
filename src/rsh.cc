3ae7330c96465a3ed478a92a06408611f0d2c027
lyuhang428 <lyuhang428@gmail.com>
2026-01-05 12:49:50 +0100

    Update rsh.cc

--- src/rsh.cc
+++ src/rsh.cc
@@ -11,14 +11,6 @@ double rsh::rsh(const int l, const int m, const double theta, const double phi) 
     if (m == 0) return P;
     if (m > 0)  return phase * root2 * P * std::cos(mm * phi);
     return phase * root2 * P * std::sin(mm * phi);
-
-    // me
-    // const int phase = std::abs(m) % 2 == 0 ? 1 : -1;
-    // double res = 0;
-    // if (m == 0) res = std::sph_legendre(l, 0, theta);
-    // else if (m > 0) res = phase * std::sqrt(2.) * std::sph_legendre(l, m, theta) * std::cos(m * phi);
-    // else if (m < 0) res = phase * std::sqrt(2.) * std::sph_legendre(l, -m, theta) * std::sin(-m * phi);
-    // return res;
 }
 
 
