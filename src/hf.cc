cbe581be21474cf29531b548ca470fd38e45adb5
lyuhang428 <lyuhang428@gmail.com>
2026-01-05 12:51:44 +0100

    Update hf.cc

--- src/hf.cc
+++ src/hf.cc
@@ -9,9 +9,9 @@ void sf::HF::diis(xxd& fock, const std::vector<xxd>& focks, const std::vector<xx
 {
     std::cout << "    DIIS ENABLED\n";
     xxd B = xxd::Zero(focks.size()+1, focks.size()+1);
-    B.bottomRows(1) = -Eigen::RowVectorXd::Ones(focks.size()+1); // B[-1, :] = -1
-    B.rightCols(1)  = -xd::Ones(focks.size()+1); // B[:, -1] = -1
-    B(B.rows()-1, B.cols()-1) = 0.; // B[-1, -1] = 0.
+    B.bottomRows(1) = -Eigen::RowVectorXd::Ones(focks.size()+1);
+    B.rightCols(1)  = -xd::Ones(focks.size()+1);
+    B(B.rows()-1, B.cols()-1) = 0.;
 
     for (auto ii=0; ii < focks.size(); ++ii) {
         for (auto jj=0; jj < focks.size(); ++jj) {
@@ -20,7 +20,7 @@ void sf::HF::diis(xxd& fock, const std::vector<xxd>& focks, const std::vector<xx
     }
 
     xd diis_rhs = xd::Zero(B.rows());
-    diis_rhs[diis_rhs.size()-1] = -1.; // diis_res[-1] = -1.
+    diis_rhs[diis_rhs.size()-1] = -1.;
     xd diis_coef = B.lu().solve(diis_rhs);
     fock *= 0.;
     for (auto iii=0; iii < diis_coef.size()-1; ++iii) {
@@ -34,13 +34,11 @@ void sf::HF::diis(xxd& fock, const std::vector<xxd>& focks, const std::vector<xx
 
 sf::HF::HF::HF(const std::string& xyzfile, const std::string& name) : xyzfile(xyzfile), name(name)
 {
-    // 在方法类内部初始化一个分子对象，而非传入一个分子对象引用
     this->mol = sf::Molecule{xyzfile, name};
 }
 
 void sf::HF::HF::energy_log() const
 {
-    // 输出 SCF 每一步的能量分解
     std::cout << "One-electron energy               " << std::fixed << std::setprecision(12) << std::left << this->kinetic_energies.back() + this->external_energies.back() << std::endl;
     std::cout << "Two-electron energy               " << std::fixed << std::setprecision(12) << std::left << this->hartree_energies.back() << std::endl;
     std::cout << "Total energy                      " << std::fixed << std::setprecision(12) << std::left << this->etots.back() << std::endl;
@@ -49,26 +47,17 @@ void sf::HF::HF::energy_log() const
 
 void sf::HF::HF::scf(int maxiter, double e_convergence, double d_convergence, int nbuffer, const std::string& initial_guess, const bool pure)
 {
-    // 构造函数中通过坐标文件和基组文件初始化分子对象
-    // 取得分子对象中的原子和壳层信息
-    // 分子类存储了纯球谐和笛卡尔两种类型的壳层，这里根据 pure 参数选择使用哪一种
     const std::vector<libint2::Atom>& atoms = this->mol.atoms;
     const std::vector<libint2::Shell>& shells = pure ? this->mol.shells_pure : this->mol.shells_cart;
     const int nbf = pure ? this->mol.nbf_pure : this->mol.nbf_cart;
-
-    // const std::vector<libint2::Shell>& shells_pure = this->mol.shells_pure;
-    // const std::vector<libint2::Shell>& shells_cart = this->mol.shells_cart;
     
     const int nshell = this->mol.nshell;
-    // const int nbf_pure = this->mol.nbf_pure;
-    // const int nbf_cart = this->mol.nbf_cart;
     const double e_nuc = this->mol.e_nuc;
 
-    std::vector<size_t> shell2bf = sf::get_shell2bf(shells);  // index
-    const size_t       max_nprim = sf::get_max_nprim(shells); // maximum number of primitive functions in all shell
+    std::vector<size_t> shell2bf = sf::get_shell2bf(shells);
+    const size_t       max_nprim = sf::get_max_nprim(shells);
     const int               lmax = sf::get_lmax(shells);
 
-    // very cheap
     const xt::xtensor<double, 2> sij = sf::get_olp(shells);
     const xt::xtensor<double, 2> tij = sf::get_kin(shells);
     const xt::xtensor<double, 2> vij = sf::get_ext(shells, atoms);
@@ -82,21 +71,18 @@ void sf::HF::HF::scf(int maxiter, double e_convergence, double d_convergence, in
     eigsolver.compute(sij_map);
     const xxd u = eigsolver.eigenvectors();
     const xd  s = eigsolver.eigenvalues();
-    const xxd inv_s = (1. / s.array()).sqrt().matrix().asDiagonal().toDenseMatrix(); // s^-1/2
-    const xxd sij_inv_half = u * inv_s * u.transpose(); // for symmetrical orthogonalization and diis residue vector
+    const xxd inv_s = (1. / s.array()).sqrt().matrix().asDiagonal().toDenseMatrix();
+    const xxd sij_inv_half = u * inv_s * u.transpose();
 
-    std::vector<xxd> focks   ; focks.reserve(maxiter+1);    // for diis
-    std::vector<xxd> diis_res; diis_res.reserve(maxiter+1); // for diis
+    std::vector<xxd> focks   ; focks.reserve(maxiter+1);
+    std::vector<xxd> diis_res; diis_res.reserve(maxiter+1);
 
-    // params setup
     int counter = 0;
     double etot_old = std::nan("1");
     xxd Puv_old = xxd::Ones(nbf, nbf) * std::nan("1");
     const int ne = this->mol.ne;
     const int nocc = this->mol.nocc;
 
-    // initial guess
-    // TODO: add SAD SAP
     xxd fock = Hcore;
 
 
@@ -117,15 +103,10 @@ void sf::HF::HF::scf(int maxiter, double e_convergence, double d_convergence, in
         xxd vecs = eigsolver2.eigenvectors();
         xxd Puv  = 2. * vecs.leftCols(nocc) * vecs.leftCols(nocc).transpose(); // 初始密度矩阵
 
-        // 计算库伦矩阵和交换矩阵
-        // DO NOT store all ERI
-        // compute (ij|kl) on the fly
-        // TODO: add Schwartz screening / density fitting
         auto [_Juv, _Kuv] = sf::get_JK(shells, Puv); // 已乘密度矩阵
         Eigen::Map<xxd> Juv{_Juv.data(), nbf, nbf};
         Eigen::Map<xxd> Kuv{_Kuv.data(), nbf, nbf};
 
-        // fock = Hcore + Juv + Kuv; fock = (fock + fock.transpose()) * 0.5;
         fock = Hcore + Juv - Kuv; fock = (fock + fock.transpose()) * 0.5;
         focks.emplace_back(fock);
         diis_res.emplace_back(sij_inv_half * (fock * Puv * sij_map - sij_map * Puv * fock) * sij_inv_half);
@@ -135,7 +116,6 @@ void sf::HF::HF::scf(int maxiter, double e_convergence, double d_convergence, in
         double kin_e = (Puv * tij_map.transpose()).trace();
         double ext_e = (Puv * vij_map.transpose()).trace();
         double hartree_e  = (Puv * Juv.transpose()).trace() * 0.5;
-        // double exchange_e = (Puv * Kuv.transpose()).trace() * 0.5;
         double exchange_e = (Puv * Kuv.transpose()).trace() * -0.5;
         double etot = 0.5 * (Puv.array() * (Hcore + fock).array()).sum() + e_nuc; // (A * B).sum() <=> (A @ B.T).trace()
 
@@ -145,7 +125,6 @@ void sf::HF::HF::scf(int maxiter, double e_convergence, double d_convergence, in
         std::cout << "One-Electron energy = " << std::setw(25) << std::setprecision(15) << std::fixed << std::right << kin_e + ext_e          << std::endl;
         std::cout << "Two-Electron energy = " << std::setw(25) << std::setprecision(15) << std::fixed << std::right << hartree_e + exchange_e << std::endl;
         std::cout << "Total energy        = " << std::setw(25) << std::setprecision(15) << std::fixed << std::right << etot                   << std::endl;
-        // std::cout << std::setprecision(15) << kin_e + ext_e + hartree_e + exchange_e + e_nuc << std::endl;
         std::cout << "Energy difference   = " << std::setw(25) << std::setprecision(15) << std::fixed << std::right << e_diff                 << std::endl;
         std::cout << "Density difference  = " << std::setw(25) << std::setprecision(15) << std::fixed << std::right << d_diff                 << std::endl;
 
