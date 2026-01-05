f0dc734bab12f267e05e1e9de44db55356ae22eb
lyuhang428 <lyuhang428@gmail.com>
2026-01-05 12:22:15 +0100

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,62 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "include/hf.hpp"
+#include "include/dft.hpp"
+
+
+void test_hf()
+{
+    const char* datadir = std::getenv("DATADIR");
+    if (datadir == nullptr) {std::cerr << "DATADIR not defined\n"; exit(-1);}
+    const std::string LDATADIR{datadir};
+    
+    std::cout << LDATADIR << std::endl;
+
+    libint2::Shell::do_enforce_unit_normalization(false); // this setup is consistent with MY-bf convention
+    libint2::initialize(); // DON'T FOGET TO INITIALIZE AND FINALIZE LIBINT2 !
+
+    std::string xyzfile = std::string{LDATADIR} + "/xyz/co2.xyz";
+    std::string name    = std::string{LDATADIR} + "/gbs/cc-pvdz.g94";
+    sf::HF::HF hf{xyzfile, name};
+
+    std::cout << "\n!!! Running HF in Spherical form !!!\n";
+    hf.scf(30, 1e-6, 1e-6, -1, "core", true);
+    
+
+    libint2::finalize();
+}
+
+void test_dft()
+{
+    const char* datadir = std::getenv("DATADIR");
+    if (datadir == nullptr) {std::cerr << "DATADIR not defined\n"; exit(-1);}
+    const std::string LDATADIR{datadir};
+
+    std::cout << LDATADIR << std::endl;
+
+    libint2::Shell::do_enforce_unit_normalization(false);
+    libint2::initialize(); // DON'T FOGET TO INITIALIZE AND FINALIZE LIBINT2 !
+
+    std::string xyzfile = std::string{LDATADIR} + "/xyz/ch3oh.xyz";
+    std::string name    = std::string{LDATADIR} + "/gbs/cc-pvdz.g94";
+    sf::DFT::DFT dft{xyzfile, name};
+
+    dft.init();
+
+
+    std::cout << "\n!!! Running DFT in Spherical form !!!\n";
+    dft.scf(30, 1e-8, 1e-8, -1, "core", 1, 8, true);
+
+    libint2::finalize();
+}
+
+
+
+int main()
+{
+    
+    test_hf();
+    std::cout << std::string(60, '-') << std::endl;
+    test_dft();
+}
