cbd67011e7e2f31ddf4d8b6a8e96d02d40c4a3b9
lyuhang428 <lyuhang428@gmail.com>
2026-03-31 23:09:05 +0200

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -4,10 +4,12 @@
 installdir=install
 builddir=build
 
-if [[ -d "$installdir" ]] ; then echo "deleting ./install folder" ; rm -rf ${installdir} ; fi
-if [[ -d "$builddir" ]] ; then echo "deleting ./build folder" ; rm -rf ${builddir} ; fi
+#if [[ -d "$installdir" ]] ; then echo "deleting ./install folder" ; rm -rf ${installdir} ; fi
+#if [[ -d "$builddir" ]] ; then echo "deleting ./build folder" ; rm -rf ${builddir} ; fi
 
-export DATADIR=${PWD}/data
+is_intel=$(whereis icpx | cut -d ':' -f 2)
+if [[ -z $is_intel ]] ; then echo 'source intel setvars' ; source /path/to/intel/oneapi/setvars.sh ; fi
+
 cmake -B build -DCMAKE_CXX_COMPILER=icpx -DCMAKE_INSTALL_PREFIX=./install
-cmake --build build/ -j8
+cmake --build build/ -j4
 cmake --install build/
