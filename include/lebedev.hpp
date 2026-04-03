eeb14904f3d3d263662e6ebd546cab3b8fbbce2b
lyuhang428 <lyuhang428@gmail.com>
2026-04-04 00:08:05 +0200

    update lebedev.hpp

--- include/lebedev.hpp
+++ include/lebedev.hpp
@@ -14,21 +14,7 @@
 
 namespace lebedev
 {
-
-/**
- * @brief 计算列别杰夫采样点和对应的权重
- * 
- * @param n    列别杰夫求积阶数 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 35, 41, 47, 53, 59, 65, 71, 77, 83, 89, 95, 101, 107, 113, 119, 125, 131
- * 
- * @return xwleb : xt::xtensor<double, 2>, (4, nanag)
- * 
- *          前三行是xyz坐标，最后一行是权重
-*/
 xt::xtensor<double, 2> lebedev_rule(const int n);
-
-
-
-
 }
 
 #endif
